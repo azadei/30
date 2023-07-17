0b18b0b1ce9493cdd5b2327af49167d12a3a9cba
azadei <azadei@code.edu.az>
2023-07-17 15:20:38 +0400

    1

---
+++ 30/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        List<string> elements = new List<string> { "Butter", "Honey", "Biscuit", "Brade", "Butter" };
+
+        
+        var arrangedList = elements.Distinct().OrderBy(element => element);
+
+        
+        Console.WriteLine("Here is the arranged list:");
+        foreach (var element in arrangedList)
+        {
+            Console.WriteLine(element);
+        }
+
+    }
+}
